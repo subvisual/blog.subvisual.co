f6c446079cb7cf8f9e0b1a1728b469a6707b5d6b
Miguel Palhas <mpalhas@gmail.com>
2013-07-15 18:30:58 +0100

    initial work on layout

---
+++ db/migrate/20130715135920_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.references :user
+      t.string :title, null: false
+      t.text   :body, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,20 @@
 users = [
-  { first_name: "Roberto", last_name: "Machado", email: "rmdmachado@groupbuddies.com", password: "rmdmachado"},
-  { first_name: "Miguel",  last_name: "Palhas",  email: "mpalhas@groupbuddies.com",    password: "mpalhas"}
+  { first_name: "Roberto", last_name: "Machado", email: "rmdmachado@groupbuddies.com"},
+  { first_name: "Miguel",  last_name: "Palhas",  email: "mpalhas@groupbuddies.com"}
 ]
+
+users.each do |user_data|
+  User.where(user_data).first_or_initialize.tap do |user|
+    user.password = user.email.split("@").first
+    user.save!
+  end
+end
+
+posts = [
+  { user_id: 1, title: "OMG a post", body: "This isn't really a good post"},
+  { user_id: 2, title: "And another, better, post", body: "Now this i like!"}
+]
+
+posts.each do |post_data|
+  Post.where(post_data).first_or_create!
+end
