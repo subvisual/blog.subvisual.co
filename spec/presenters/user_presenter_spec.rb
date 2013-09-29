d3216abb185561ec9b72f7d8b7af50b9343a58de
Miguel Palhas <mpalhas@gmail.com>
2013-09-29 02:03:03 +0100

    adds a test

--- spec/presenters/user_presenter_spec.rb
+++ spec/presenters/user_presenter_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
+require 'uri'
 
 describe UserPresenter do
 
   context "#photo" do
-    it "return an image tag based on user first name" do
+    it "returns an image tag based on user first name" do
       user_presenter = UserPresenter.new(build(:user, first_name: :dummy), view)
 
       user_presenter.photo.should include 'dummy.png'
+    end
+  end
+
+  context "#twitter_url" do
+    it "gives a correct twitter url" do
+      user_presenter = UserPresenter.new(build(:user), view)
+
+      user_presenter.twitter_url.should match URI::regexp
     end
   end
 end
