444402f46f1d472e11956d7f3c780d1785952f32
Miguel Palhas <mpalhas@gmail.com>
2013-10-07 11:06:53 +0100

    photos now use full name

--- spec/presenters/user_presenter_spec.rb
+++ spec/presenters/user_presenter_spec.rb
@@ -3,11 +3,19 @@ require 'uri'
 
 describe UserPresenter do
 
+  context "#full_name" do
+    it "strips leading or trailing whitespace" do
+      user_presenter = UserPresenter.new(build(:user, first_name: ' first', last_name: 'last '), view)
+
+      user_presenter.full_name.should eq 'first last'
+    end
+  end
+
   context "#photo" do
     it "returns an image tag based on user first name" do
-      user_presenter = UserPresenter.new(build(:user, first_name: :dummy), view)
+      user_presenter = UserPresenter.new(build(:user, first_name: :foo, last_name: :bar), view)
 
-      user_presenter.photo.should include 'dummy.png'
+      user_presenter.photo.should include 'foo_bar.png'
     end
   end
 
