603fef7edf2fd763d29df2a9755874fedf5e5395
Miguel Palhas <mpalhas@gmail.com>
2014-12-04 17:34:59 +0000

    Points to the blog server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server 'yoda.servers.groupbuddies.com', user: 'deploy', roles: %w{web app db}, primary: true
+server 'blog.groupbuddies.com', user: 'deploy', roles: %w{web app db}, primary: true
 set :deploy_to, '/var/www/gb-blog'
