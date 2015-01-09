88a39b6a9fdc636c052a7cc22411a3fad63b25db
Miguel Palhas <mpalhas@gmail.com>
2015-01-09 15:48:30 +0000

    Separates user and admin layouts

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,9 +1,7 @@
 class Admin::ApplicationController < ::ApplicationController
-  decorates_assigned :posts, :user
+  layout 'admin'
 
-  def admin_controller?
-    true
-  end
+  decorates_assigned :posts, :user
 
   def dashboard
     @posts = Post.visible_by current_user
