8c911c7337cc31c0977548d27a7d573a6d5bfaf5
Miguel Palhas <mpalhas@gmail.com>
2015-02-11 18:59:42 +0000

    Show all posts in the archive

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -4,7 +4,7 @@ class ArchivesController < ApplicationController
   decorates_assigned :posts
 
   def show
-    @posts = Post.published.recent
+    @posts = Post.published
     filter_by_category
     filter_by_author
     @archive = Facades::Archive.new(@posts.decorate)
