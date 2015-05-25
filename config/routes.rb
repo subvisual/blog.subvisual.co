8f2a087e744c698643b82383f1daf52f1c37eb26
Miguel Palhas <mpalhas@gmail.com>
2015-05-25 15:05:06 +0100

    Scoped to /blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,42 @@
 GbBlog::Application.routes.draw do
+  scope '/blog' do
+    root to: "posts#index"
 
-  root to: "posts#index"
+    resources :users, controller: :users, only: [:create]
+    resources :posts, only: [:show]
 
-  resources :users, controller: :users, only: [:create]
-  resources :posts, only: [:show]
-
-  scope controller: :archive do
-    get '/archive' => 'archives#show', as: :archive
-    get '/author/:author' => 'archives#show', as: :author
-    get '/category/:category' => 'archives#show', as: :category
-  end
-
-  get '/feed' => 'posts#feed'
-
-  namespace :admin do
-    get '/', to: 'application#dashboard', as: :dashboard
-
-    resources :posts, only: [:index, :show, :new, :create, :edit, :update] do
-      patch :publish
-      patch :unpublish
+    scope controller: :archive do
+      get '/archive' => 'archives#show', as: :archive
+      get '/author/:author' => 'archives#show', as: :author
+      get '/category/:category' => 'archives#show', as: :category
     end
-    resources :post_images, only: [:create]
 
-    get 'me', to: 'users#show', as: :me
-    patch 'me', to: 'users#update', as: :update_me
-    resources :users, only: [:show, :update]
+    get '/feed' => 'posts#feed'
 
-    post 'saying', to: 'application#create_saying', as: :create_saying
+    namespace :admin do
+      get '/', to: 'application#dashboard', as: :dashboard
+
+      resources :posts, only: [:index, :show, :new, :create, :edit, :update] do
+        patch :publish
+        patch :unpublish
+      end
+      resources :post_images, only: [:create]
+
+      get 'me', to: 'users#show', as: :me
+      patch 'me', to: 'users#update', as: :update_me
+      resources :users, only: [:show, :update]
+
+      post 'saying', to: 'application#create_saying', as: :create_saying
+    end
+
+    namespace :api, defaults: { format: :json }, constraints: { format: :json } do
+      resources :posts, only: [:create, :update]
+    end
+
+    scope controller: :pages do
+      get :ui_kit
+    end
+
+    mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   end
-
-  namespace :api, defaults: { format: :json }, constraints: { format: :json } do
-    resources :posts, only: [:create, :update]
-  end
-
-  scope controller: :pages do
-    get :ui_kit
-  end
-
-  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 end
