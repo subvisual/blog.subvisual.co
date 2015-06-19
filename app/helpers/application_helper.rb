810c8873ca36b4ab92979fd94d8b81fed1215249
Miguel Palhas <mpalhas@gmail.com>
2015-06-19 18:29:26 +0100

    Fixes navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def nav_item(text, url, options = {})
+    options[:class] ||= ''
+    options[:class] << ' Nav-item'
+
+    content_tag :li, options do
+      link_to text, url, class: 'Nav-link'
+    end
+  end
+
+  def nav_overlay_item(text, url, options = {})
+    options[:class] ||= ''
+    options[:class] << ' NavOverlay-item'
+
+    content_tag :li, options do
+      link_to text, url, class: 'NavOverlay-link'
+    end
+  end
 end
