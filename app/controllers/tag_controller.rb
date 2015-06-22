8c665beb3064a99544ee1f1d2280fadd186ec2f5
Miguel Palhas <mpalhas@gmail.com>
2015-06-22 17:37:47 +0100

    Redirects from tag page if no posts are found

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -3,5 +3,9 @@ class TagController < ApplicationController
   def show
     @tag = params[:tag]
     @posts = Post.published.tagged_with(@tag)
+
+    if @posts.none?
+      redirect_to posts_path
+    end
   end
 end
