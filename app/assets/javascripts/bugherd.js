5a855624eade5c02edeef3bb55e1cbc829e1cc02
Miguel Palhas <mpalhas@gmail.com>
2015-06-22 16:34:13 +0100

    Fixes lints

--- app/assets/javascripts/bugherd.js
+++ app/assets/javascripts/bugherd.js
@@ -1,5 +1,6 @@
-(function (d, t) {
-  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+(function(d, t) {
+  var bh = d.createElement(t);
+  var s = d.getElementsByTagName(t)[0];
   bh.type = 'text/javascript';
   bh.src = '//www.bugherd.com/sidebarv2.js?apikey=wvjvaxxdl56qeuzqhmda3g';
   s.parentNode.insertBefore(bh, s);
