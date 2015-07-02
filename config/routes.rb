bb2c06eec59ceb98e9f6c200f62c44c951adfbc5
Miguel Palhas <mpalhas@gmail.com>
2015-07-02 13:57:39 +0100

    Prevents wrong formats

--- config/routes.rb
+++ config/routes.rb
@@ -3,44 +3,48 @@ GbBlog::Application.routes.draw do
   get '/blog/auth/:provider/callback', to: 'sessions#create'
 
   scope '/blog' do
-    root to: "posts#index"
+    constraints format: :html do
+      root to: "posts#index"
 
-    resources :posts, only: %i(index show) do
-      collection do
-        resources :tag, only: [:show], param: :tag
-        get :search
+      resources :posts, only: %i(index show) do
+        collection do
+          resources :tag, only: [:show], param: :tag
+          get :search
+        end
       end
+
+      resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
+      resource :session, controller: 'clearance/sessions', only: [:create]
+      resources :users, controller: 'clearance/users', only: [] do
+        resource :password, controller: 'clearance/passwords', only: %i(create edit update)
+      end
+
+      get '/sign_in' => 'clearance/sessions#new', as: 'sign_in'
+      delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
+      get '/sign_up' => 'clearance/users#new', as: 'sign_up'
+
+      namespace :admin do
+        get '/', to: 'application#dashboard', as: :dashboard
+
+        resources :posts, except: %i(destroy) do
+          patch :publish
+          patch :unpublish
+        end
+
+        resources :users, only: [:edit, :update]
+      end
+
+      mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
     end
 
     defaults format: :rss do
       get '/feed' => 'feed#index', as: :feed
     end
 
-    resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
-    resource :session, controller: 'clearance/sessions', only: [:create]
-    resources :users, controller: 'clearance/users', only: [] do
-      resource :password, controller: 'clearance/passwords', only: %i(create edit update)
-    end
-
-    get '/sign_in' => 'clearance/sessions#new', as: 'sign_in'
-    delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
-    get '/sign_up' => 'clearance/users#new', as: 'sign_up'
-
     namespace :admin do
-      get '/', to: 'application#dashboard', as: :dashboard
-
-      resources :posts, except: %i(destroy) do
-        patch :publish
-        patch :unpublish
-      end
-
-      resources :users, only: [:edit, :update]
-
       defaults format: :json do
         resources :post_images, only: [:create]
       end
     end
-
-    mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   end
 end
