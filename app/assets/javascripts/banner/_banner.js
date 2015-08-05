666647a73e123d0b7025bdbe38dc479a503cba18
João Justo <jpjustonunes@hotmail.com>
2015-08-05 11:10:24 +0100

    Better animation

--- app/assets/javascripts/banner/_banner.js
+++ app/assets/javascripts/banner/_banner.js
@@ -1,52 +1,58 @@
 $(function(){
   window.subvisual = window.subvisual || {};
   window.subvisual.banner = (function() {
-    var $element = $('.Banner');
+    var $banner = $('.Banner');
 
-    function putBellowNav() {
-      if ($element.hasClass('Banner--bellowNav')) return;
-
-      $element.removeClass('Banner--show');
-      $element.removeClass('Banner--animateTop');
-
-      $element.addClass('Banner--bellowNav');
-    }
-
-    function putOnTop() {
-      console.log('onTop');
-      if ($element.hasClass('Banner--animateTop')) return;
-
-      if ($element.hasClass('Banner--bellowNav')) {
-        $element.removeClass('Banner--bellowNav');
-        $element.addClass('Banner--animateTop');
-      } else {
-        ($element.addClass('Banner--show'));
-      }
-    }
-
-    function removeBanner() {
-      console.log('remove');
-
+    function clearState($element) {
+      $element.removeClass('Banner--hide');
       $element.removeClass('Banner--show');
       $element.removeClass('Banner--bellowNav');
       $element.removeClass('Banner--animateTop');
     }
 
-    function updateWithScroll(scrollState, $element) {
-      if (scrollState.hasntScrolledEnough())
-        return;
+    function putBellowNav($element) {
+      if ($element.hasClass('Banner--bellowNav')) return;
 
-      if (scrollState.isAtTheTop()) {
-        removeBanner();
+      clearState($element);
+      $element.addClass('Banner--bellowNav');
+    }
+
+    function putOnTop($element) {
+      if ($element.hasClass('Banner--animateTop')) return;
+
+      if ($element.hasClass('Banner--bellowNav')) {
+        clearState($element);
+        $element.addClass('Banner--animateTop');
+      } else {
+        clearState($element);
+        $element.addClass('Banner--show');
+      }
+    }
+
+    function hide($element) {
+      if ($element.hasClass('Banner--hide')) return;
+
+      if (!$element.is('.Banner--show, .Banner--animateTop, .Banner--bellowNav')) return;
+
+      clearState($element);
+      $element.addClass('Banner--hide');
+    }
+
+    function updateWithScroll(scrollState) {
+      var hasPassedTheElement = scrollState.hasPassedTheElement($banner.outerHeight());
+
+      if (!hasPassedTheElement) {
+        hide($banner);
         return;
       }
 
-      var hasPassedTheElement = scrollState.hasPassedTheElement($element.outerHeight());
+      if (scrollState.hasntScrolledEnough())
+        return;
 
       if (scrollState.hasScrolledDown()) {
-        putOnTop();
+        putOnTop($banner);
       } else {
-        putBellowNav();
+        putBellowNav($banner);
       }
     }
 
