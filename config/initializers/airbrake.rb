8708daf61963750994c80f7485ef328f17a69a88
Miguel Palhas <mpalhas@gmail.com>
2015-10-20 10:26:31 +0100

    Airbrake only in production

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,8 @@
-Airbrake.configure do |config|
-  config.api_key = ENV['AIRBRAKE_API_KEY']
-  config.host = 'log.subvisual.co'
-  config.port = 443
-  config.secure = (config.port == 443)
+if Rails.env.production?
+  Airbrake.configure do |config|
+    config.api_key = ENV['AIRBRAKE_API_KEY']
+    config.host = 'log.subvisual.co'
+    config.port = 443
+    config.secure = (config.port == 443)
+  end
 end
