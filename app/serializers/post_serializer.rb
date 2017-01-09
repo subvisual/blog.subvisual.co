8d0b1214f7eaec788df9cdd9e701c0d37486e489
Gabriel Poça <gabrielpoca@gmail.com>
2017-01-09 15:10:39 +0000

    Quickfix: add path to PostSerializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,10 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :tag_list, :processed_intro
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :title, :tag_list, :processed_intro, :path
   belongs_to :author
+
+  def path
+    post_path(object)
+  end
 end
